c79b2588ae974f4b9a99c3551b16ae0ee5373263
NEU-ZYXi <stephenxi@hotmail.com>
2018-12-04 12:57:49 -0800

    Update 2. Add Two Numbers.java

--- Math/2. Add Two Numbers.java
+++ Math/2. Add Two Numbers.java
@@ -16,25 +16,25 @@ Explanation: 342 + 465 = 807.
 */
 
 public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode prev = dummy;
-        int sum = 0;
-        while (l1 != null || l2 != null) {
-            sum /= 10;
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            prev.next = new ListNode(sum % 10);
-            prev = prev.next;
+    ListNode dummy = new ListNode(0);
+    ListNode prev = dummy;
+    int sum = 0;
+    while (l1 != null || l2 != null) {
+        sum /= 10;
+        if (l1 != null) {
+            sum += l1.val;
+            l1 = l1.next;
         }
-        if (sum >= 10) prev.next = new ListNode(1);
-        return dummy.next;
+        if (l2 != null) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        prev.next = new ListNode(sum % 10);
+        prev = prev.next;
     }
+    if (sum >= 10) prev.next = new ListNode(1);
+    return dummy.next;
+}
 
 
 
