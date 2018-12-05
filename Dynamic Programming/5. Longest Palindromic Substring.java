0b382dd73ec1a34e3892a64fadedae8b2b118661
NEU-ZYXi <stephenxi@hotmail.com>
2018-12-04 23:51:51 -0800

    Update 5. Longest Palindromic Substring.java

--- Dynamic Programming/5. Longest Palindromic Substring.java
+++ Dynamic Programming/5. Longest Palindromic Substring.java
@@ -14,7 +14,27 @@ Output: "bb"
 
 */
 
+/*
 
+Solution: dp[i][j]=true, if s[i]=s[j] and dp[i+1][j-1]=true which means s[i+1...j-1] is palindromic
+O(n^2),O(n^2)
+
+*/
+
+public String longestPalindrome(String s) {
+    int n = s.length();
+    String ans = "";
+    boolean[][] dp = new boolean[n][n];
+    for (int j = 0; j < n; ++j) {
+        for (int i = j; i >= 0; --i) {
+            if (s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1]))
+                dp[i][j] = true;
+            if (dp[i][j] && ans.length() < j - i + 1) 
+                ans = s.substring(i, j + 1);
+        }
+    }
+    return ans;
+}
 
 
 
