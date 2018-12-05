74e3243482fc42911d3480387d98012c16864d7c
NEU-ZYXi <stephenxi@hotmail.com>
2018-12-05 15:58:57 -0800

    Update 6. ZigZag Conversion.java

--- String/6. ZigZag Conversion.java
+++ String/6. ZigZag Conversion.java
@@ -27,3 +27,31 @@ P     I
 
 
 */
+
+/*
+
+O(n),O(row)
+
+*/
+
+public String convert(String s, int numRows) {
+    if (numRows == 1) return s;
+    StringBuilder[] sb = new StringBuilder[numRows];
+    for (int i = 0; i < numRows; ++i) 
+        sb[i] = new StringBuilder();
+    int idx = 0, delta = 1;
+    for (int i = 0; i < s.length(); ++i) {
+        sb[idx].append(s.charAt(i));
+        if (idx == 0) delta = 1;
+        if (idx == numRows - 1) delta = -1;
+        idx += delta;
+    }
+    String ans = "";
+    for (int i = 0; i < numRows; ++i) 
+        ans += sb[i];
+    return ans;
+}
+
+
+
+
