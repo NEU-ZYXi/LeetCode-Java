f9104413f8f440db155eb0d6a85e28ee4f0a83b0
NEU-ZYXi <stephenxi@hotmail.com>
2018-12-10 14:51:25 -0800

    Create 28. Implement strStr().java

---
+++ String/28. Implement strStr().java
@@ -0,0 +1,35 @@
+
+/*
+
+Implement strStr().
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "hello", needle = "ll"
+Output: 2
+
+Example 2:
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+*/
+
+/*
+
+O(n^2),O(1)
+
+*/
+
+public int strStr(String haystack, String needle) {
+    for (int i = 0; ; ++i) {
+        for (int j = 0; ; ++j) {
+            if (j == needle.length()) return i;
+            if (i + j == haystack.length()) return -1;
+            if (haystack.charAt(i + j) != needle.charAt(j)) break;
+        }
+    }
+}
+
+
+
+
