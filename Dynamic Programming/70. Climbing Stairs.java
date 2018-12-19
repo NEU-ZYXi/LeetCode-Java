788954b526e5f2b49135280d1b07e93dbbf2aa9a
NEU-ZYXi <stephenxi@hotmail.com>
2018-12-18 17:11:39 -0800

    Create 70. Climbing Stairs.java

---
+++ Dynamic Programming/70. Climbing Stairs.java
@@ -0,0 +1,58 @@
+
+/*
+
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+Note: Given n will be a positive integer.
+
+Example 1:
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+*/
+
+/*
+
+O(n),O(n)
+
+*/
+
+public int climbStairs(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    int[] dp = new int[n];
+    dp[0] = 1;
+    dp[1] = 2;
+    for (int i = 2; i < n; ++i) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n - 1];
+}
+
+
+public int climbStairs(int n) {
+    int first = 0, second = 1, ans = 0;
+    for (int i = 0; i < n; ++i) {
+        ans = first + second;
+        first = second;
+        second = ans;
+    }
+    return ans;
+}
+
+
+
+
+
