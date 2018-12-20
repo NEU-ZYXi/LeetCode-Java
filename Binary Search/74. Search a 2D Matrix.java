5091083cf81da12b834080ff527bf73309ca6978
NEU-ZYXi <stephenxi@hotmail.com>
2018-12-19 16:27:10 -0800

    Update 74. Search a 2D Matrix.java

--- Binary Search/74. Search a 2D Matrix.java
+++ Binary Search/74. Search a 2D Matrix.java
@@ -57,16 +57,16 @@ O(n+m),O(1)
 */
 
 public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix.length == 0) return false;
-        int n = matrix.length, m = matrix[0].length;
-        int i = 0, j = m - 1;
-        while (i < n && j >= 0) {
-            if (matrix[i][j] == target) return true;
-            if (matrix[i][j] > target) j--;
-            else i++;
-        }
-        return false;
+    if (matrix.length == 0) return false;
+    int n = matrix.length, m = matrix[0].length;
+    int i = 0, j = m - 1;
+    while (i < n && j >= 0) {
+        if (matrix[i][j] == target) return true;
+        if (matrix[i][j] > target) j--;
+        else i++;
     }
+    return false;
+}
 
 
 
