34031bf4cc92b1c8ca674a6e824dfc0b9136d38b
NEU-ZYXi <stephenxi@hotmail.com>
2018-12-20 20:26:47 -0800

    Create 89. Grey Code.java

---
+++ Bit Manipulation/89. Grey Code.java
@@ -0,0 +1,51 @@
+
+/*
+
+The gray code is a binary numeral system where two successive values differ in only one bit.
+Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. 
+A gray code sequence must begin with 0.
+
+Example 1:
+Input: 2
+Output: [0,1,3,2]
+Explanation:
+00 - 0
+01 - 1
+11 - 3
+10 - 2
+For a given n, a gray code sequence may not be uniquely defined.
+For example, [0,2,3,1] is also a valid gray code sequence.
+00 - 0
+10 - 2
+11 - 3
+01 - 1
+
+Example 2:
+Input: 0
+Output: [0]
+Explanation: We define the gray code sequence to begin with 0.
+             A gray code sequence of n has size = 2n, which for n = 0 the size is 20 = 1.
+             Therefore, for n = 0 the gray code sequence is [0].
+
+*/
+
+/*
+
+Solution: 0 -> 00 01 11 10, add 1 to each answer in thr previous step, add from back to promise only one digit is different
+O(n*2^n),O(1)
+
+*/
+
+public List<Integer> grayCode(int n) {
+    List<Integer> ans = new ArrayList<>();
+    ans.add(0);
+    for (int i = 0; i < n; ++i) {
+        int sz = ans.size();
+        for (int j = sz - 1; j >= 0; --j) ans.add(ans.get(j) + (1 << i));
+    }
+    return ans;
+}
+
+
+
+
