883ab427d6b1068ea1bdb51a47756f87f8a06052
NEU-ZYXi <stephenxi@hotmail.com>
2019-01-24 17:20:31 -0800

    Create 147. Insertion Sort List.java

---
+++ Linked List/147. Insertion Sort List.java
@@ -0,0 +1,46 @@
+
+/*
+
+Sort a linked list using insertion sort.
+
+A graphical example of insertion sort. The partial sorted list (black) initially contains only the first element in the list.
+With each iteration one element (red) is removed from the input data and inserted in-place into the sorted list
+ 
+Algorithm of Insertion Sort:
+Insertion sort iterates, consuming one input element each repetition, and growing a sorted output list.
+At each iteration, insertion sort removes one element from the input data, 
+finds the location it belongs within the sorted list, and inserts it there.
+It repeats until no input elements remain.
+
+Example 1:
+Input: 4->2->1->3
+Output: 1->2->3->4
+
+Example 2:
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+
+*/
+
+/*
+
+O(n)~O(n^2),O(1)
+
+*/
+
+public ListNode insertionSortList(ListNode head) {
+    ListNode dummy = new ListNode(0);
+    ListNode prev = dummy, cur = head, nxt = null;
+    while (cur != null) {
+        nxt = cur.next;
+        if (prev.val >= cur.val) prev = dummy;
+        while (prev.next != null && prev.next.val < cur.val) prev = prev.next;
+        cur.next = prev.next;
+        prev.next = cur;
+        cur = nxt;
+    }
+    return dummy.next;
+}
+
+
+
