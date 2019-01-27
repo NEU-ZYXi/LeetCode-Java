9446fc64b31718135c32a930698dedd934e2b1d5
NEU-ZYXi <stephenxi@hotmail.com>
2019-01-26 17:59:30 -0800

    Create 157. Read N Characters Given Read4.java

---
+++ String/157. Read N Characters Given Read4.java
@@ -0,0 +1,86 @@
+
+/*
+
+Given a file and assume that you can only read the file using a given method read4, implement a method to read n characters.
+
+Method read4:
+The API read4 reads 4 consecutive characters from the file, then writes those characters into the buffer array buf.
+The return value is the number of actual characters read.
+Note that read4() has its own file pointer, much like FILE *fp in C.
+Definition of read4:
+    Parameter:  char[] buf
+    Returns:    int
+Note: buf[] is destination not source, the results from read4 will be copied to buf[]
+Below is a high level example of how read4 works:
+File file("abcdefghijk"); // File is "abcdefghijk", initially file pointer (fp) points to 'a'
+char[] buf = new char[4]; // Create buffer with enough space to store characters
+read4(buf); // read4 returns 4. Now buf = "abcd", fp points to 'e'
+read4(buf); // read4 returns 4. Now buf = "efgh", fp points to 'i'
+read4(buf); // read4 returns 3. Now buf = "ijk", fp points to end of file
+ 
+Method read:
+By using the read4 method, the method read that reads n characters from the file and store it in the buffer array buf. 
+Consider that you cannot manipulate the file directly.
+The return value is the number of actual characters read.
+Definition of read:
+    Parameters:	char[] buf, int n
+    Returns:	int
+Note: buf[] is destination not source, you will need to write the results to buf[]
+ 
+Example 1:
+Input: file = "abc", n = 4
+Output: 3
+
+Example 2:
+Input: file = "abcde", n = 5
+Output: 5
+Explanation: After calling your read method, buf should contain "abcde". 
+We read a total of 5 characters from the file, so return 5.
+
+Example 3:
+Input: file = "abcdABCD1234", n = 12
+Output: 12
+Explanation: After calling your read method, buf should contain "abcdABCD1234". 
+We read a total of 12 characters from the file, so return 12.
+
+Example 4:
+Input: file = "leetcode", n = 5
+Output: 5
+Explanation: After calling your read method, buf should contain "leetc". 
+We read a total of 5 characters from the file, so return 5.
+
+*/
+
+/*
+
+O(n),O(4)
+
+*/
+
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char[] buf);
+ */
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    public int read(char[] buf, int n) {
+        int ans = 0;
+        char[] tmp = new char[4];
+        boolean eof = false;
+        while (!eof && ans < n) {
+            int cnt = read4(tmp);
+            if (cnt < 4) eof = true;
+            int i = 0;
+            while (ans < n && i < cnt) buf[ans++] = tmp[i++];
+        }
+        return ans;
+    }
+}
+
+
+
+
