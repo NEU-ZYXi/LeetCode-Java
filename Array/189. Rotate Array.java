07c3b4d6100c0a444297a4c66276a8c5a3d58b73
NEU-ZYXi <stephenxi@hotmail.com>
2019-01-31 21:43:19 -0800

    Create 189. Rotate Array.java

---
+++ Array/189. Rotate Array.java
@@ -0,0 +1,64 @@
+
+/*
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+*/
+
+/*
+
+Solution 1: reverse in 3 steps
+O(n),O(1)
+
+*/
+
+public void rotate(int[] nums, int k) {
+    int n = nums.length;
+    k %= n;
+    reverse(nums, 0, n - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, n - 1);
+}
+
+private void reverse(int[] nums, int i, int j) {
+    while (i < j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+        i++;
+        j--;
+    }
+}
+
+
+/*
+
+Solution 2: use an extra array
+O(n),O(n)
+
+*/
+
+public void rotate(int[] nums, int k) {
+    int n = nums.length;
+    k %= n;
+    int[] clone = nums.clone();
+    for (int i = 0; i < n; ++i) nums[(i + k) % n] = clone[i];
+}
+
+
+
