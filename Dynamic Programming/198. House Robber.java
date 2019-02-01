2465d7df4c1f99ecb34bbf954e4784bfd2d6ece9
NEU-ZYXi <stephenxi@hotmail.com>
2019-02-01 12:36:19 -0800

    Create 198. House Robber.java

---
+++ Dynamic Programming/198. House Robber.java
@@ -0,0 +1,57 @@
+
+/*
+
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that 
+adjacent houses have security system connected and it will automatically contact the police 
+if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, 
+determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+             
+Example 2:
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+
+*/
+
+/*
+
+Solution: dp[i] means the maximum value of robbery for nums[0...i]
+          dp[i]=max(dp[i-1],dp[i-2]+nums[i]), which means the two cases of robbing the current one or not
+O(n),O(n)          
+
+*/
+
+public int rob(int[] nums) {
+    int n = nums.length;
+    if (n == 0) return 0;
+    if (n == 1) return nums[0];
+    int[] dp = new int[n];
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    for (int i = 2; i < n; ++i) dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    return dp[n - 1];
+}
+
+
+public int rob(int[] nums) {
+    int prev = 0, cur = 0, ans = 0;
+    for (int i = 0; i < nums.length; ++i) {
+        ans = Math.max(cur, prev + nums[i]);
+        prev = cur;
+        cur = ans;
+    }
+    return ans;
+}
+
+
+
