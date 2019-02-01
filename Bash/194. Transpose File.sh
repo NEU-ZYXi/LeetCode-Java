169485c7c82f6a52ae73eb9091b677fe1ff7c338
NEU-ZYXi <stephenxi@hotmail.com>
2019-02-01 12:15:36 -0800

    Create 194. Transpose File.sh

---
+++ Bash/194. Transpose File.sh
@@ -0,0 +1,42 @@
+
+: '
+
+Given a text file file.txt, transpose its content.
+You may assume that each row has the same number of columns and each field is separated by the ' ' character.
+
+Example:
+If file.txt has the following content:
+name age
+alice 21
+ryan 30
+Output the following:
+name alice ryan
+age 21 30
+
+'
+
+: '
+
+NF: a variable indicating the number of fields (i.e. number of "columns") on an input line
+NR: a variable indicating the number of records (i.e. current line number) that's accumulated across multiple files read
+$i: the i-th field of the input line
+
+'
+
+awk '
+{
+    for (i = 1; i <= NF; i++) {
+        if(NR == 1) {
+            s[i] = $i;
+        } else {
+            s[i] = s[i] " " $i;
+        }
+    }
+}
+END {
+    for (i = 1; s[i] != ""; i++) {
+        print s[i];
+    }
+}' file.txt
+
+
