1db86b8a8af5116beb8d3158844053da238cb160
NEU-ZYXi <stephenxi@hotmail.com>
2019-05-10 16:13:31 -0700

    Create 454. 4Sum II.java

---
+++ Hash Table/454. 4Sum II.java
@@ -0,0 +1,50 @@
+
+/*
+
+Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are 
+such that A[i] + B[j] + C[k] + D[l] is zero.
+To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. 
+All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+
+Example:
+Input:
+A = [ 1, 2]
+B = [-2,-1]
+C = [-1, 2]
+D = [ 0, 2]
+Output:
+2
+Explanation:
+The two tuples are:
+1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+
+*/
+
+/*
+
+O(n^2),O(n)
+
+*/
+
+public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+    Map<Integer, Integer> map = new HashMap<>();
+    int ans = 0;
+    for (int a : A) {
+        for (int b : B) {
+            int sum = a + b;
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+    }
+    for (int c : C) {
+        for (int d : D) {
+            int sum = -(c + d);
+            ans += map.getOrDefault(sum, 0);
+        }
+    }
+    return ans;
+}
+
+
+
+
