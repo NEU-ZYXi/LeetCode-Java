ed882489a8ec406b26727e087833352b172bf823
NEU-ZYXi <stephenxi@hotmail.com>
2019-05-28 17:25:35 -0400

    Create 545. Boundary of Binary Tree.java

---
+++ Tree/545. Boundary of Binary Tree.java
@@ -0,0 +1,105 @@
+
+/*
+
+Given a binary tree, return the values of its boundary in anti-clockwise direction starting from root. 
+Boundary includes left boundary, leaves, and right boundary in order without duplicate nodes.  
+(The values of the nodes may still be duplicates.)
+Left boundary is defined as the path from root to the left-most node. 
+Right boundary is defined as the path from root to the right-most node. 
+If the root doesn't have left subtree or right subtree, then the root itself is left boundary or right boundary. 
+Note this definition only applies to the input binary tree, and not applies to any subtrees.
+The left-most node is defined as a leaf node you could reach when you always firstly travel to the left subtree if exists. 
+If not, travel to the right subtree. Repeat until you reach a leaf node.
+The right-most node is also defined by the same way with left and right exchanged.
+
+Example 1
+Input:
+  1
+   \
+    2
+   / \
+  3   4
+Ouput:
+[1, 3, 4, 2]
+Explanation:
+The root doesn't have left subtree, so the root itself is left boundary.
+The leaves are node 3 and 4.
+The right boundary are node 1,2,4. Note the anti-clockwise direction means you should output reversed right boundary.
+So order them in anti-clockwise without duplicates and we have [1,3,4,2].
+
+Example 2
+Input:
+    ____1_____
+   /          \
+  2            3
+ / \          / 
+4   5        6   
+   / \      / \
+  7   8    9  10  
+Ouput:
+[1,2,4,7,8,9,10,6,3]
+Explanation:
+The left boundary are node 1,2,4. (4 is the left-most node according to definition)
+The leaves are node 4,7,8,9,10.
+The right boundary are node 1,3,6,10. (10 is the right-most node).
+So order them in anti-clockwise without duplicate nodes we have [1,2,4,7,8,9,10,6,3].
+
+*/
+
+/*
+
+O(n),O(logn)
+
+*/
+
+public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+    List<Integer> ans = new ArrayList<>();
+    if (root == null) {
+        return ans;
+    }
+    ans.add(root.val);
+    getLeft(root.left, ans);
+    getLeaves(root.left, ans);
+    getLeaves(root.right, ans);
+    getRight(root.right, ans);
+    return ans;
+}
+
+private void getLeft(TreeNode root, List<Integer> ans) {
+    if (root == null || (root.left == null && root.right == null)) {
+        return;
+    }
+    ans.add(root.val);
+    if (root.left != null) {
+        getLeft(root.left, ans);
+    } else {
+        getLeft(root.right, ans);
+    }
+}
+
+private void getRight(TreeNode root, List<Integer> ans) {
+    if (root == null || (root.left == null && root.right == null)) {
+        return;
+    }
+    if (root.right != null) {
+        getRight(root.right, ans);
+    } else {
+        getRight(root.left, ans);
+    }
+    ans.add(root.val);
+}
+
+private void getLeaves(TreeNode root, List<Integer> ans) {
+    if (root == null) {
+        return;
+    }
+    if (root.left == null && root.right == null) {
+        ans.add(root.val);
+    }
+    getLeaves(root.left, ans);
+    getLeaves(root.right, ans);
+}
+
+
+
+
