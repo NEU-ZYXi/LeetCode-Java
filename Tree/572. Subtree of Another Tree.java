7ecdfe09aaea2a63c0844ab002a00d1e5d3e600b
NEU-ZYXi <stephenxi@hotmail.com>
2019-06-01 11:44:08 -0400

    Create 572. Subtree of Another Tree.java

---
+++ Tree/572. Subtree of Another Tree.java
@@ -0,0 +1,66 @@
+
+/*
+
+Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. 
+A subtree of s is a tree consists of a node in s and all of this node's descendants. 
+The tree s could also be considered as a subtree of itself.
+
+Example 1:
+Given tree s:
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+   4 
+  / \
+ 1   2
+Return true, because t has the same structure and node values with a subtree of s.
+
+Example 2:
+Given tree s:
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+Given tree t:
+   4
+  / \
+ 1   2
+Return false.
+
+*/
+
+/*
+
+O(n^2),O(logn)
+
+*/
+
+public boolean isSubtree(TreeNode s, TreeNode t) {
+    if (s == null) {
+        return false;
+    }
+    if (isSame(s, t)) {
+        return true;
+    }
+    return isSubtree(s.left, t) || isSubtree(s.right, t);
+}
+
+private boolean isSame(TreeNode s, TreeNode t) {
+    if (s == null || t == null) {
+        return s == t;
+    }
+    if (s.val != t.val) {
+        return false;
+    }
+    return isSame(s.left, t.left) && isSame(s.right, t.right);
+}
+
+
+
+
