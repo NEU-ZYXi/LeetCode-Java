57fe5ef518e4fe880eb0cf60ac036815b64df90b
NEU-ZYXi <stephenxi@hotmail.com>
2019-09-07 16:24:12 -0700

    Create 704. Binary Search.java

---
+++ Binary Search/704. Binary Search.java
@@ -0,0 +1,42 @@
+
+/*
+
+Given a sorted (in ascending order) integer array nums of n elements and a target value, 
+write a function to search target in nums. If target exists, then return its index, otherwise return -1.
+
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+*/
+
+/*
+
+O(logn),O(1)
+
+*/
+
+public int search(int[] nums, int target) {
+    int l = 0, r = nums.length - 1;
+    while (l < r) {
+        int mid = (l + r) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+    return nums[l] == target ? l : -1;
+}
+
+
+
+
